d1f86aeb4c885c593ed8e53b4ed30dab0ed5b05d
Neo Colpin <colpinneo@gmail.com>
2025-04-02 15:51:34 +0200

    ajout: commentaires et exemple widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+
+//widgets
 import '../widgets/flow_charts.dart';
+import '../widgets/test_widget.dart';
+//modeles d'organisation des données
 import '../models/flow_observation.dart';
+//Communication avec l'API
 import '../services/hub_eau_flow.dart';
 
+
+// Démarrage de l'application
 void main() {
   runApp(const MyApp());
 }
-//commentaire
+
+
+// Classe principale de l'application
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HubEau Stations',
+      title: 'HubEau Stations', //nom de l'onglet / application
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -22,6 +31,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+// Page d'accueil de l'application
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -29,15 +40,19 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+// Etat de la page d'accueil
 class _MyHomePageState extends State<MyHomePage> {
   late Future<List<FlowObservation>> _futureObservations;
 
+  // Initialisation des données par défaut de la page
   @override
   void initState() {
     super.initState();
     _futureObservations = HubEauFlow().getFlowByStationAndDate('O919001001', '2025-03-30');
   }
 
+  // Affichage des widgets
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,22 +60,29 @@ class _MyHomePageState extends State<MyHomePage> {
       body: FutureBuilder<List<FlowObservation>>(
         future: _futureObservations,
         builder: (context, snapshot) {
+
+          // Gestion des différents états/erreurs de chargement des données
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
+          }
+          else if (snapshot.hasError) {
             return Center(child: Text('Erreur : ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          }
+          else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Center(child: Text('Aucune donnée disponible.'));
           }
 
+          // Récupération des données à partir de l'API
           List<FlowObservation> observations = snapshot.data!;
           List<FlowObservation> hauteurData = filterByType(observations, "H");
           List<FlowObservation> debitData = filterByType(observations, "Q");
 
+          // Affichage des widgets
           return ListView(
             children: [
               FlowChart(observations: hauteurData, type: "H"),
               FlowChart(observations: debitData, type: "Q"),
+              TestWidget(),
             ],
           );
         },
